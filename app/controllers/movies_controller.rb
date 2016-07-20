5a248446001b348dccd7b763f3003d73e38ff261
wedgey <desmondpoon5@hotmail.com>
2016-07-20 08:10:49 +0000

    Implemented search form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,18 @@
 class MoviesController < ApplicationController
   def index
     @movies = Movie.all
+    
+    @movies = @movies.where("title like ?", "%#{search_params[:title]}%") unless search_params[:title].nil?
+    @movies = @movies.where("director like ?", "%#{search_params[:director]}%") unless search_params[:director].nil?
+    case search_params[:duration]
+    when 'Under 90 minutes'
+      @movies = @movies.where("runtime_in_minutes < 90")
+    when 'Between 90 and 120 minutes'
+      @movies = @movies.where("runtime_in_minutes BETWEEN 90 AND 120")
+    when 'Over 120 minutes'
+      @movies = @movies.where("runtime_in_minutes > 120")
+    end
+    # @movies = Movie.where(search_params)
   end
 
   def show
@@ -46,6 +58,12 @@ class MoviesController < ApplicationController
   def movie_params
     params.require(:movie).permit(
       :title, :release_date, :director, :runtime_in_minutes, :poster_image_url, :description
+    )
+  end
+
+  def search_params
+    params.permit(
+      :title, :director, :duration
     )
   end
 
