4039e30efadee028b15059afa5775574c1cd2905
wedgey <desmondpoon5@hotmail.com>
2016-07-20 21:02:25 +0000

    Convert sessions resource to a singular resource

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   resources :users
 
-  resources :sessions, only: [:new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
 
   namespace :admin do 
     resources :users
